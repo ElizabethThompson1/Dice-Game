69f0b446676f7d06af00651aa1ae5c839f8a9b73
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-09-07 09:58:29 -0500

    to the addedeventlistener i created a if else statment that would add the rolls to the players score after every click it could see who had active attached to it and add that to the message dom this would be changed every roll once i created the scores i made a if else stament that checked if it was or more than 20 if it did that player won

--- index.js
+++ index.js
@@ -6,13 +6,38 @@ const message = document.getElementById("message");
 const rollBtn = document.getElementById("rollBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-let turn = true
-let scoreOne = ""
-let scoreTwo = ""
+let player1Turn = true
+let scoreOne = 0
+let scoreTwo = 0
 
 rollBtn.addEventListener("click", function () {
-    let roll = Math.floor(Math.random() * 6)
+    const roll = Math.floor(Math.random() * 6) + 1
     console.log(roll);
+    if (player1Turn === true) {
+        scoreOne += roll
+        score1Board.textContent = scoreOne
+        diceOne.textContent = roll
+        diceOne.classList.remove("active")
+        diceTwo.classList.add("active")
+        message.textContent = "Player 2 Turn"
+
+
+    } else {
+        scoreTwo += roll
+        diceTwo.textContent = roll
+        score2Board.textContent = scoreTwo
+        diceTwo.classList.remove("active")
+        diceOne.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+
+    if (scoreOne >= 20) {
+        message.textContent = "Player 1 Wins"
+    } else if (scoreTwo >= 20) {
+        message.textContent = "Player 2 Wins"
+    }
+
+    player1Turn = !player1Turn
 
 })
 
